2b85aa1aa6eca2dbe81b76f78571beaa0243a2da
KumaR SonU <122993102+KumarSonu01@users.noreply.github.com>
2025-07-30 01:35:28 +0530

    Update script.js
    function added for night mode toggle button

--- script.js
+++ script.js
@@ -136,3 +136,16 @@ icons.forEach(icon => {
         }
     });
 });
+
+const toggle = document.getElementById('theme-toggle');
+      if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+        toggle.textContent = "☀️";
+      }
+
+      toggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        const isDark = document.body.classList.contains("dark-mode");
+        toggle.textContent = isDark ? "☀️" : "🌙";
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+      });
